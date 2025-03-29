b347d5e4515df6cf690918d77916e2bfc8fe0914
Alerson Rigo <alersonrigo@Alersons-MacBook-Pro.local>
2025-03-29 01:02:55 -0300

    app

---
+++ build/precache-manifest.436cc350b091ec69d7a67735c6ed439f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25d7fe39984b6b817b21c0fb207b049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dde5dac2673c164f676",
+    "url": "/static/css/main.be10013a.chunk.css"
+  },
+  {
+    "revision": "5ef2ef029cc6066e12cd",
+    "url": "/static/js/2.f48db819.chunk.js"
+  },
+  {
+    "revision": "0e70cb57d9eff2ad4cf363da0809cb30",
+    "url": "/static/js/2.f48db819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200699514bd419490ff6",
+    "url": "/static/js/3.8b02ade8.chunk.js"
+  },
+  {
+    "revision": "1dde5dac2673c164f676",
+    "url": "/static/js/main.80a4200c.chunk.js"
+  },
+  {
+    "revision": "007045c10ba514cd203d",
+    "url": "/static/js/runtime-main.b0b5eb18.js"
+  }
+]);
