ab181d44eac2efbc186221350cd3e0c8c6ddd213
Alerson Rigo <alersonrigo@Alersons-MacBook-Pro.local>
2025-04-14 19:03:33 -0300

    nova funcionalidade

--- src/components/SeletorCategoria/SeletorCategoria.jsx
+++ src/components/SeletorCategoria/SeletorCategoria.jsx
@@ -5,25 +5,39 @@ import { MensagensContext } from '../../context/MensagensContext';
 const SeletorCategoria = () => {
   const { categoriaAtual, selecionarCategoria } = useContext(MensagensContext);
 
+  // Definindo os ícones (emojis) para cada categoria
+  const icones = {
+    familia: '👨‍👩‍👧‍👦',
+    criancas: '👶',
+    valores: '🧠',
+    motivacao: '✨'
+  };
+
   return (
     <div className="seletor-categoria">
       <button 
         className={`categoria-botao ${categoriaAtual === 'familia' ? 'ativo' : ''}`}
         onClick={() => selecionarCategoria('familia')}
       >
-        Família
+        <span className="icone-categoria">{icones.familia}</span> Família
       </button>
       <button 
         className={`categoria-botao ${categoriaAtual === 'criancas' ? 'ativo' : ''}`}
         onClick={() => selecionarCategoria('criancas')}
       >
-        Crianças
+        <span className="icone-categoria">{icones.criancas}</span> Crianças
       </button>
       <button 
         className={`categoria-botao ${categoriaAtual === 'valores' ? 'ativo' : ''}`}
         onClick={() => selecionarCategoria('valores')}
       >
-        Valores
+        <span className="icone-categoria">{icones.valores}</span> Valores
+      </button>
+      <button 
+        className={`categoria-botao ${categoriaAtual === 'motivacao' ? 'ativo' : ''}`}
+        onClick={() => selecionarCategoria('motivacao')}
+      >
+        <span className="icone-categoria">{icones.motivacao}</span> Motivação
       </button>
     </div>
   );
